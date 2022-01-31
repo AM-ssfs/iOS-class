50bffc3af91b46ebc14936ef609e64ddce684587
AM-ssfs <menscad@ssfs.org>
2022-01-31 09:46:02 -0500

    controlFlow

--- 1 - Getting Started/4 - Control Flow/Lab - Control Flow.playground/Pages/3. App Exercise - Fitness Decisions.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/4 - Control Flow/Lab - Control Flow.playground/Pages/3. App Exercise - Fitness Decisions.xcplaygroundpage/Contents.swift
@@ -5,9 +5,21 @@
  
  You want your fitness tracking app to give as much encouragement as possible to your users. Create a variable `steps` equal to the number of steps you guess you've taken today. Create a constant `stepGoal` equal to 10,000. Write an if-else statement that will print "You're almost halfway there!" if `steps` is less than half of `stepGoal`, and will print "You're over halfway there!" if `steps` is greater than half of `stepGoal`.
  */
-
-
+var steps = 1234
+let stepGoal = 10000
+var text = steps > stepGoal/2 ? "You're over halfway there!" : "You're almost halfway there!"
+print(text)
 //:  Now create a new, but similar, if-else-if statement that prints "Way to get a good start today!" if `steps` is less than a tenth of `stepGoal`, prints "You're almost halfway there!" if `steps` is less than half of `stepGoal`, and prints "You're over halfway there!" if `steps` is greater than half of `stepGoal`.
+switch steps {
+case 0...stepGoal/10:
+    print("Way to get a good start today!")
+case stepGoal/10...stepGoal/2:
+    print("You're almost halfway there!")
+case stepGoal/2...stepGoal:
+    print("You're over halfway there!")
+default:
+    print("")
+}
 
 
 /*:
