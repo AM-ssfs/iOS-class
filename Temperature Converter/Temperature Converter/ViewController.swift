9d4ee828f4a12b81f7c21a7ac1c33e5f74d77b12
AM-ssfs <menscad@ssfs.org>
2022-02-23 09:55:27 -0500

    Temp converter done?

--- Temperature Converter/Temperature Converter/ViewController.swift
+++ Temperature Converter/Temperature Converter/ViewController.swift
@@ -11,38 +11,60 @@ class ViewController: UIViewController {
     
     var isFahrenheit = true
     
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     
     }
+    @IBOutlet weak var ConvertText: UIButton!
     
     @IBOutlet weak var textEntry: UITextField!
     
     @IBOutlet weak var farCelOutlet: UISwitch!
+
     
     @IBAction func farCelSwitch(_ sender: Any) {
         isFahrenheit.toggle()
         farCelOutlet.setOn(isFahrenheit, animated: true)
         print(isFahrenheit)
+        convertButtonText()
+        
     }
+    
+    
+    func convertButtonText () {
+        if isFahrenheit == true{
+            ConvertText.setTitle("Convert to celcius", for: .normal)
+        }
+        else{
+            ConvertText.setTitle("Convert to fahrenheit", for: .normal)
+        }
+    }
+
     
     @IBAction func ButtonPress(_ sender: Any) {
         if isFahrenheit{
             isFahrenheit = false
             farCelOutlet.setOn(isFahrenheit, animated: true)
             var textAsDouble = Double(textEntry.text!)
-            textAsDouble = ( (textAsDouble! - 32) * 5/9 )
-            textAsDouble = String(textAsDouble)
+            textAsDouble = ( (textAsDouble! - 32) * (5/9) )
+            textAsDouble = Double(Int(textAsDouble!*100))/100
             textEntry.text = String(textAsDouble!)
+            convertButtonText()
+            //ConvertText.setTitle("Convert to celcius", for: .normal)
         }
         else{
             isFahrenheit = true
             farCelOutlet.setOn(isFahrenheit, animated: true)
             var textAsDouble = Double(textEntry.text!)
-            textAsDouble = ( (textAsDouble! * 5/9 ) + 32 )
+            textAsDouble = ( (textAsDouble! * (9/5) ) + 32 )
+            textAsDouble = Double(Int(textAsDouble!*100))/100
             textEntry.text = String(textAsDouble!)
+            convertButtonText()
+            //ConvertText.setTitle("Convert to fahrenheit", for: .normal)
         }
     }
 }
