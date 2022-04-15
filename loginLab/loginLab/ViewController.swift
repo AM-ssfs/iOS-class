36e2e3234bfe64ac9b69160ee43cb68fa1788344
Aden <menscad@ssfs.org>
2022-04-15 10:43:42 -0400

    loginLab

---
+++ loginLab/loginLab/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  loginLab
+//
+//  Created by Aden M on 4/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        <#code#>
+    }
+
+}
+
