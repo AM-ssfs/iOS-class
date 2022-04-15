4d4f41bfd1fe8ee95a303290e1f94a1c3607194b
Aden <menscad@ssfs.org>
2022-04-15 10:57:48 -0400

    loginLab

--- loginLab/loginLab/ViewController.swift
+++ loginLab/loginLab/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        <#code#>
+        
     }
 
 }
