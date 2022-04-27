514fdf74099aa75ea8eed4fadcc819b918fdbfc3
Aden <menscad@ssfs.org>
2022-04-27 11:50:39 -0400

    meal counter

---
+++ Meal Tracker/Food.swift
@@ -0,0 +1,8 @@
+//
+//  Food.swift
+//  Meal Tracker
+//
+//  Created by Aden M on 4/27/22.
+//
+
+import Foundation
---
+++ Meal Tracker/Meal.swift
@@ -0,0 +1,8 @@
+//
+//  Meal.swift
+//  Meal Tracker
+//
+//  Created by Aden M on 4/27/22.
+//
+
+import Foundation
