200680255c07fb39a63bd5fd0e4de8b4531173ee
Aden <menscad@ssfs.org>
2022-05-04 09:39:46 -0400

    table view

--- Meal Tracker/FoodTableViewController.swift
+++ Meal Tracker/FoodTableViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class FoodTableViewController: UITableViewController {
-    var meals: [Meal] = [Meal(name: "breakfast", food: [Food(name: "egg", description: "scrambled"), Food(name: "toast", description: "not burnt"), Food(name: "juice", description: "orange")]),
-    Meal(name: "lunch", food: [Food(name: "apple", description: "fresh"), Food(name: "", description: ""), Food(name: "", description: "")]
-        )]
+    var meals: [Meal] = [
+        Meal(name: "breakfast", food: [Food(name: "egg", description: "scrambled"), Food(name: "toast", description: "not burnt"), Food(name: "juice", description: "orange")]),
+        Meal(name: "lunch", food: [Food(name: "apple", description: "fresh"), Food(name: "sandwich", description: "smoked turkey"), Food(name: "fries", description: "sweet-potato")]),
+        Meal(name: "dinner", food: [Food(name: "soup", description: "tomato"), Food(name: "chicken", description: "fried"), Food(name: "noodles", description: "spicy")])
+            
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,20 +29,20 @@ class FoodTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return meals.count // number of meals
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return meals[ section ].food.count // number of food in that meal
     }
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Food", for: indexPath)
 
         // Configure the cell...
-
+        indexPath.section
         return cell
     }
 
