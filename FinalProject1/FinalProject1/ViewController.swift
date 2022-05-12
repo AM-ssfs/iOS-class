c2e10f85c537efee7d5f6cae4b6fc5fc68dfa8e8
Aden <menscad@ssfs.org>
2022-05-12 08:59:57 -0400

    in the middle of implementing table view

--- FinalProject1/FinalProject1/ViewController.swift
+++ FinalProject1/FinalProject1/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+
+    // https://programmingwithswift.com/create-a-uitableview-with-swift/
+    let tableViewData = Array(repeating: "Item", count: 5)
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "TableViewCell")
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 
 
